79d3562b7ad1f946eafe7b1be21cb7c3937558af
loudeco <louise.decombe@laplateforme.io>
2021-08-17 14:12:04 +0200

    add config dropzone and firebase

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# dropit_vue_firebase
-VueJS dropzone, images hosted with firebase
+# dropzone
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+
+</template>
+
+<script>
+import firebase from 'firebase';
+import vue2Dropzone from 'vue2-dropzone';
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+let uuid = require("uuid");
+
+export default {
+  name: 'App',
+  components: {
+    vueDropzone: vue2Dropzone;
+  },
+  data(){
+    return{
+      images: [],
+      dropzoneOptions: {
+        url:"https://httpbin.org/post",
+        thumbnailWidth: 250,
+        thumbnailHeight: 250,
+        addRemoveLinks: true,
+        acceptedFiles: ".jpg, .jpeg, .png"
+      }
+    }
+  },
+
+methods: {
+  async afterComplete(file) {
+    try {
+      
+      const imageName = uuid.v1();
+      const metadata = {
+        contentType: "image/png"
+      }
+
+      const storageRef = firebase.storage().ref; 
+      const imageref = storageRef.child()
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+},
+
+}
+</script>
+
+<style>
+
+</style>
